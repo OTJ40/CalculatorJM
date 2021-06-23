b105a657c97455d7225972e46be53a767bb6e508
OTJ40 <81230439+OTJ40@users.noreply.github.com>
2021-06-23 14:43:49 +0300

    upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class Main {
+    public static void main(String[] args) {
+        Input input = new Input();
+        //System.out.printf("%s %s%n", input.getA(), input.getB());
+        new Calculator(input);
+        //calculator.outputResult(input.isRoman());
+    }
+}
+
